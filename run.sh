55440dc8270c4ee2acdffc931a22726a0d3e1002
wsq <445433732@qq.com>
2019-11-29 22:22:12 +0800

    fix: repair CA sign

--- run.sh
+++ run.sh
@@ -3,21 +3,27 @@ set -e
 
 mkdir -p /root/CA
 cd /root/CA
+cp /home/workspace/CA/*.cnf /root/CA
+touch index.txt
+echo '01' > serial
+touch index.txt.attr
 
 openssl genrsa -passout pass:123456 -des3 -out myCA.key 2048
 
-openssl req -x509 -new -nodes -key myCA.key -sha256 -days 1825 -out myCA.pem -passin pass:123456 -subj "/C=CN/ST=London/L=London/O=webSecurity/OU=websecurity/CN=example.com"
+openssl req -x509 -new -nodes -key myCA.key -sha256 -days 1825 -out myCA.crt -passin pass:123456 -subj "/C=CN/ST=London/L=London/O=webSecurity/OU=websecurity/CN=localhost"
 
-cp myCA.pem /home/workspace/
+cp myCA.crt /home/workspace/
 
 openssl genrsa -out websecurity.key 2048
 
-openssl req -new -key websecurity.key -out websecurity.csr -subj "/C=CN/ST=London/L=London/O=webSecurity/OU=websecurity/CN=example.com"
+openssl req -new -key websecurity.key -out websecurity.csr -subj "/C=CN/ST=London/L=London/O=webSecurity/OU=websecurity/CN=localhost"
 
-openssl x509 -req -passin pass:123456 -in websecurity.csr -CA myCA.pem -CAkey myCA.key -CAcreateserial -out websecurity.crt -days 1825 -sha256 
+openssl ca -batch -passin pass:123456 -config ca.cnf -out websecurity.crt -extfile extensions.cnf -in websecurity.csr
 
 cd -
+
 a2enmod ssl
+
 service apache2 start
 while true
 do
